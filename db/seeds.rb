de6be10a442beb658ceff0ec86bd6c885fdfecb0
Bali Tok <bali.tok@hotmail.com>
2023-03-08 20:14:14 +0800

    Added 7 CRUD actions to web app

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+puts 'Deleting all Flats......'
+Flat.destroy_all
+
+puts 'Creating Flats....'
+10.times do
+  Flat.create!(name: Faker::Lorem.sentence, address: Faker::Address.full_address, description: Faker::Lorem.paragraph,
+               price_per_night: (100..200).to_a.sample, number_of_guests: (1..5).to_a.sample)
+end
+
+puts 'Created Flats!'
+
+# t.string "name", null: false
+# t.string "address", null: false
+# t.text "description", null: false
+# t.integer "price_per_night", null: false
+# t.integer "number_of_guests", null: false
